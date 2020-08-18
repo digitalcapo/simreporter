d8a2fd96bcc2d7006bda0e8492ede67d34a073c7
Capo Digital <hola@capo.digital>
2020-08-18 04:21:37 -0300

    first montaner
    - fixed bad os list dir method
    - makes a contact sheet but memory error

---
+++ montaner.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os.path
+import sys
+from time import strftime
+from PIL import Image
+
+row_size = 4
+margin = 0
+
+def generate_montage(filenames, output_fn):
+    Image.MAX_IMAGE_PIXELS = 1000000000
+    images = [Image.open(filename) for filename in filenames]
+
+    width = max(int(image.size[0]) + margin for image in images)*row_size
+    height = sum(int(image.size[1]) + margin for image in images)
+    montage = Image.new(mode='RGB', size=(width, height), color=(0,0,0,0))
+
+    max_x = 0
+    max_y = 0
+    offset_x = 0
+    offset_y = 0
+    for i,image in enumerate(images):
+        montage.paste(image, (offset_x, offset_y))
+
+        max_x = max(max_x, offset_x + image.size[0])
+        max_y = max(max_y, offset_y + image.size[1])
+
+        if i % row_size == row_size-1:
+            offset_y = max_y + margin
+            offset_x = 0
+        else:
+            offset_x += margin + image.size[0]
+
+    montage = montage.crop((0, 0, max_x, max_y))
+    montage.save(output_fn)
--- pexelpusher.py
+++ pexelpusher.py
@@ -7,8 +7,8 @@ api_key = argonaut.loadThis('api_key.json')[0]
 import os
 from pypexels import PyPexels
 import requests
-import PythonMagick
-
+import contactsheet
+import montaner
 
 # instantiate PyPexels object
 
@@ -17,9 +17,9 @@ class PexelPusher:
     def __init__(self):
         self.py_pexel = PyPexels(api_key=api_key)
 
-    def searchVideos(self, query=''):
+    def searchVideos(self, query='', qSize=10):
         selected_videos = []
-        search_videos_page = self.py_pexel.videos_search(query=query, per_page=35)
+        search_videos_page = self.py_pexel.videos_search(query=query, per_page=qSize)
         while True:
             for video in search_videos_page.entries:
                 #print(video.id, video.user.get('name'), video.url)
@@ -30,10 +30,10 @@ class PexelPusher:
             search_videos_page = search_videos_page.get_next_page()
         return selected_videos
 
-    def getThumbs(self, querylist):
+    def getThumbs(self, querylist,qSize):
         nlist = 0
         for query in querylist:
-            video_list = self.searchVideos(query)
+            video_list = self.searchVideos(query, qSize)
             nlist = nlist+1
             cache_folder = ('./cacheimages/{:04d}/').format(nlist)
             if not os.path.exists(cache_folder):
@@ -46,17 +46,22 @@ class PexelPusher:
                     outfile.close()
 
     def makeContactSheet(self):
+        root = './cacheimages/'
         folderlist = []
-        for folder in os.dir.list('./cacheimages/'):
+        rfiles = []
+        rfilename = []
+        for folder in os.listdir(root):
             folderlist.append(folder)
-        for folder in folderlist:
-            rfiles = []
-            for file in os.dir.list(folder):
-                print(file)
+            rfilename.append((os.path.join(root,folder,'cs_{0}.jpg')).format(folder))
+        for sfolder in folderlist:
+            for file in os.listdir('./cacheimages/'+sfolder):
+                rfiles.append(os.path.join('./cacheimages',sfolder,file))
+        return rfiles, rfilename[0]
 
 
 if __name__ == '__main__':
     ppusher = PexelPusher()
-    query = ['urban+night','city+night','city','buildings']
-    ppusher.getThumbs(query)
-    ppusher.makeContactSheet()
+    query = ['urban+night']
+    ppusher.getThumbs(query, 10)
+    rfiles, rfolder = ppusher.makeContactSheet()
+    montaner.generate_montage(rfiles, rfolder)
